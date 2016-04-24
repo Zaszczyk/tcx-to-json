e13c56a2db566319d680df6836bdc6af5265637a
Mateusz Błaszczyk <mateusz.blaszczyk@live.com>
2016-04-25 00:36:41 +0200

    first test

--- src/MateuszBlaszczyk/TcxToJson/Parser.php
+++ src/MateuszBlaszczyk/TcxToJson/Parser.php
@@ -9,7 +9,23 @@
 namespace MateuszBlaszczyk\TcxToJson;
 
 
+use XMLReader;
+
 class Parser
 {
+    protected $xml;
+
+    public function __construct($xml)
+    {
+        $this->xml = $xml;
+    }
+
+    public function parse()
+    {
+        $xmlReader = new XMLReader;
+        $xmlReader->xml($this->xml);
+
+        return true;
+    }
 
 }
